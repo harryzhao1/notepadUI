d3f5757674739f006ed40c18e3925ec15d86e38e
Keith Zhou <keiby.zhou@gmail.com>
2013-10-12 13:39:01 -0400

    Initial Commit

---
+++ NBPad/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NBPad
+//
+//  Created by Keith Zhou on 2013-10-12.
+//  Copyright (c) 2013 keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
